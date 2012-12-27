139367d4e32e11fa1d6756474b34b195bf7efcf1
Jisse Reitsma <info@yireo.com>
2012-12-27 12:10:37 +0100

    Update

--- joomla25/modules/mod_simplelists_items/mod_simplelists_items.php
+++ joomla25/modules/mod_simplelists_items/mod_simplelists_items.php
@@ -12,7 +12,7 @@
 defined('_JEXEC') or die('Restricted access');
 
 // Include the helper
-require_once (dirname(__FILE__).DS.'helper.php');
+require_once (dirname(__FILE__).'/helper.php');
 
 // Fetch the list of items
 $list = modSimpleListsItemsHelper::getList($params);
@@ -23,12 +23,12 @@ $style = $params->get('style', 'default');
 // Add a stylesheet per style
 if($style == 'advanced') {
     $template = JFactory::getApplication()->getTemplate();
-    if(file_exists(JPATH_SITE.DS.'templates'.DS.$template.DS.'css'.DS.'mod_simplelists_items'.DS.$style.'.css')) {
-        JHTML::stylesheet(JURI::root().'templates'.DS.$template.DS.'css'.DS.'mod_simplelists_items'.DS.$style.'.css');
-    } elseif(file_exists( JPATH_SITE.DS.'media'.DS.'mod_simplelists_items'.DS.'css'.DS.$style.'.css')) {
-        JHTML::stylesheet(JURI::root().'media'.DS.'mod_simplelists_items'.DS.'css'.DS.$style.'.css');
+    if(file_exists(JPATH_SITE.'/templates/'.$template.'/css/mod_simplelists_items/'.$style.'.css')) {
+        JHTML::stylesheet(JURI::root().'templates/'.$template.'/css/mod_simplelists_items/'.$style.'.css');
+    } elseif(file_exists( JPATH_SITE.'/media/mod_simplelists_items/css/'.$style.'.css')) {
+        JHTML::stylesheet(JURI::root().'media/mod_simplelists_items/css/'.$style.'.css');
     }
 }
 
 // Display the output
-require(JModuleHelper::getLayoutPath('mod_simplelists_items', $style));
+require JModuleHelper::getLayoutPath('mod_simplelists_items', $style);
