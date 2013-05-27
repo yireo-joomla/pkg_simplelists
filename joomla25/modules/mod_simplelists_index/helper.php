a417934c7f0bf2b907b6997492fba830cac421d5
Jisse Reitsma <info@yireo.com>
2013-05-27 17:00:35 +0200

    Pending changes

---
+++ joomla25/modules/mod_simplelists_index/helper.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author Yireo
+ * @copyright Copyright (C) 2013 Yireo
+ * @license GNU/GPL
+ * @link http://www.yireo.com/
+*/
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+/*
+ * Helper class
+ */
+class modSimpleListsIndexHelper
+{
+    /*
+     * Method to get a list of items
+     */
+	public function getItems(&$params)
+	{
+        include_once JPATH_SITE.'/components/com_simplelists/models/items.php' ;
+        $model = new SimplelistsModelItems();
+        return $model->getData();
+    }
+}
---
+++ joomla25/modules/mod_simplelists_index/mod_simplelists_index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Yireo
+ * @copyright Copyright (C) 2013 Yireo
+ * @license GNU/GPL
+ * @link http://www.yireo.com/
+*/
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+// Deny output
+if(JRequest::getCmd('option') != 'com_simplelists') return;
+if(JRequest::getCmd('view') != 'items') return;
+
+// Include the helper
+require_once (dirname(__FILE__).DS.'helper.php');
+
+// Fetch the list of items
+$items = modSimpleListsIndexHelper::getItems($params);
+
+// Display the output
+require(JModuleHelper::getLayoutPath('mod_simplelists_index'));
