f06dc1d1b7aa5bbb6aeb389e066eaa121e8f1c60
Jisse Reitsma <info@yireo.com>
2013-12-19 14:43:40 +0100

    Fix minor PHP-notice when no items are found

--- joomla25/components/com_simplelists/views/items/tmpl/picture.php
+++ joomla25/components/com_simplelists/views/items/tmpl/picture.php
@@ -14,9 +14,9 @@ defined('_JEXEC') or die('Restricted access');
 <?php echo $this->loadTemplate('_header'); ?>
 <?php 
 $column_mode = $this->params->get('column_mode', 'css');
-$columns = $this->params->get('columns');
+$columns = (int)$this->params->get('columns');
 if(!$columns > 0) $columns = 4;
-if(count($this->items) < $columns) $columns = count($this->items);
+if(count($this->items) < $columns && count($this->items) > 0) $columns = count($this->items);
 
 if($column_mode == 'bootstrap') {
     $this->page_class .= ' row-fluid';
