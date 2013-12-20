a41582369c19c92fe1960a62c65b34672798ed21
Jisse Reitsma <info@yireo.com>
2013-12-20 10:50:44 +0100

    Picture-container was shown in default-layout, even when picture was empty

--- joomla25/components/com_simplelists/views/items/tmpl/default.php
+++ joomla25/components/com_simplelists/views/items/tmpl/default.php
@@ -21,11 +21,13 @@ defined('_JEXEC') or die('Restricted access');
 
         <div class="<?php echo $item->class; ?>">
 
+            <?php if(!empty($item->picture)) : ?>
             <div class="image" style="float:<?php echo $item->picture_alignment; ?>">
                 <?php if($item->picture): ?>
                 <?php echo $item->picture; ?>
                 <?php endif; ?>
             </div>
+            <?php endif; ?>
 
             <div class="body">
                 <?php if($item->title): ?>
