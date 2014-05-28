987a504cb32571bb2e9b8a2fd2efa078b0a607ab
Jisse Reitsma <info@yireo.com>
2014-05-28 08:30:08 +0200

    Move Joomla! sources to source folder

--- joomla25/administrator/components/com_simplelists/sql/uninstall.mysql.utf8.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS #__simplelists;
-DROP TABLE IF EXISTS #__simplelists_items;
-DROP TABLE IF EXISTS #__simplelists_categories;
-DROP TABLE IF EXISTS #__simplelists_plugins;
-DELETE FROM #__categories WHERE extension = "com_simplelists";
