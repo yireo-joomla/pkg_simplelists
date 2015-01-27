c52fff018f08e514eca2582a3131065a127401d4
Jisse Reitsma <info@yireo.com>
2015-01-27 09:01:55 +0100

    Fix menu

---
+++ source/administrator/components/com_simplelists/helpers/simplelists.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Joomla! component SimpleLists
+ *
+ * @author Yireo
+ * @copyright Copyright 2015 Yireo.com
+ * @license GNU General Public License
+ * @link http://www.yireo.com/
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+// Require the Yireo loader
+require_once JPATH_ADMINISTRATOR.'/components/com_simplelists/lib/loader.php';
+require_once __DIR__.'/helper.php';
+
+/**
+ * Simplelists Helper
+ * 
+ * @package Joomla
+ * @subpackage Simplelists
+ */
+class SimplelistsHelperSimplelists extends SimplelistsHelper
+{
+}
